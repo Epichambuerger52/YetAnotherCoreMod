c033b17d96c53cd806fa7c08d1eac1280b6f44c5
jensen69 <67147309+jensen69@users.noreply.github.com>
2022-07-10 00:59:57 +0700

    ver 0.0.1

---
+++ src/main/java/com/Epichaburger52/yacm/YetAnotherrCoreMod.java
@@ -0,0 +1,35 @@
+package com.Epichaburger52.yacm;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(YetAnotherrCoreMod.MOD_ID)
+public class YetAnotherrCoreMod
+{
+    public static final String MOD_ID = "yacm";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public YetAnotherrCoreMod()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
