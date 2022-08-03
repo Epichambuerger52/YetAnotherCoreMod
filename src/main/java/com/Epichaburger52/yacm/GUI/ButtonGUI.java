4557b8b2bfa3a70678c7b3bce0b29db8ac84f7ac
jensen69 <67147309+jensen69@users.noreply.github.com>
2022-08-04 01:38:49 +0700

    added ui buttons

---
+++ src/main/java/com/Epichaburger52/yacm/GUI/ButtonGUI.java
@@ -0,0 +1,16 @@
+package com.Epichaburger52.yacm.GUI;
+
+import com.Epichaburger52.yacm.YetAnotherrCoreMod;
+import net.minecraft.resources.ResourceLocation;
+
+public class ButtonGUI extends MainGUI
+{
+    final ResourceLocation GUI = new ResourceLocation(YetAnotherrCoreMod.MOD_ID,"textures/gui/gui.png");
+
+    int buttonWidth = 16;
+    int buttonHeight = 14;
+    int u = 175;
+    int v = 1;
+
+
+}
--- src/main/java/com/Epichaburger52/yacm/GUI/MainGUI.java
+++ src/main/java/com/Epichaburger52/yacm/GUI/MainGUI.java
@@ -3,7 +3,8 @@ package com.Epichaburger52.yacm.GUI;
 import com.Epichaburger52.yacm.YetAnotherrCoreMod;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
@@ -11,17 +12,19 @@ import net.minecraft.resources.ResourceLocation;
 public class MainGUI extends Screen {
 
     final ResourceLocation GUI = new ResourceLocation(YetAnotherrCoreMod.MOD_ID,"textures/gui/gui.png");
-    int HEIGHT = 202;
-    int WIDTH = 145;
+    final ResourceLocation GUIBUTTON = new ResourceLocation(YetAnotherrCoreMod.MOD_ID,"textures/gui/guibutton.png");
+    int HEIGHT = 200;
+    int WIDTH = 142;
     int relHEIGHT =(height - HEIGHT)/2 ;
     int relWIDTH= (width - WIDTH)/2 ;
-
+    private Button addstr;
 
 public  MainGUI()
 {
 
 
     super(new TranslatableComponent("main gui"));
+
 }
 
 
@@ -33,11 +36,14 @@ public  MainGUI()
         int relHEIGHT =(height - HEIGHT)/2 ;
         int relWIDTH= (width - WIDTH)/2 ;
 
-        blit(pPoseStack,relWIDTH,relHEIGHT,0,0,HEIGHT,WIDTH);
+        blit(pPoseStack,relWIDTH,relHEIGHT,0,0,WIDTH,HEIGHT);
 
 
 
 
+        addRenderableWidget(new Button(relHEIGHT,relWIDTH,10,10,new TranslatableComponent("yacm.menu.button"), pButton -> {
+
+        }));
 
 
         super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
